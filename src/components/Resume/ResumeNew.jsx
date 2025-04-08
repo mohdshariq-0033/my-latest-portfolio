dee9cafd860bd720008a634e0557e3039011442d
mohd shariq <mohdshariq0033@gmail.com>
2025-04-08 23:13:11 +0530

    resume Add

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../assets/shariq resume.pdf";
+import pdf from "/shariqResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
+  
   return (
     <>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+
+        <img src="/resumeIMG.png" className="img-fluid" alt="resumeIMG">
+        </img>
+        <Row style={{ justifyContent: "center", position: "relative", marginTop: "30px" }}>
           <Button id="Btn"
             variant="primary"
             href={pdf}
